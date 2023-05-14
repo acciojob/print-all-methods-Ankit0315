6ccfc10182fe55b6bd8eb02236dd2b5e91eb5bde
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-05-14 16:51:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //your JS code here. If required.
 function allMethods() {
   //write your code here
+	return Object.getOwnPropertyNames(obj).filter(function(property) {
+        return typeof obj[property] == "function";
 }
 
-alert(allMethods());
+ alert(allMethods());
+	
